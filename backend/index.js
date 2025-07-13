3e32a3d00e9199dc6640c096f21a30f4345ed084
IvanSarapura <sarapuraivan25@gmail.com>
2025-07-13 08:11:52 -0300

    scripts

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const { ethers } = require("ethers");
 
-const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+const provider = new ethers.JsonRpcProvider(
+  "https://sepolia.infura.io/v3/a3abdc1def32488499e3e416c908452e"
+);
 
-const contractAddress = "0x8464135c8F25Da09e49BC8782676a84730C318bC"; //Address del contrato desplegado vía REMIX
+const contractAddress = "0x9fe8fb3d466fE5e7f9A58fe32f90e12dcd2960a0"; //Address del contrato desplegado vía REMIX
 
 const abi = ["event VRF()"];
 
 async function main() {
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
+  // Escucha el evento VRFAsk
   contract.on("VRF", () => {
     console.log("Recibi un evento");
   });
---
+++ scripts/writePlayer.js
@@ -0,0 +1,11 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  addr_ = "0x9fe8fb3d466fe5e7f9a58fe32f90e12dcd2960a0"; // Sabe donde ir a buscarlo pero no tiene el ABI
+  Lottery = await ethers.getContractFactory("Lottery"); // Tiene el ABI pero nosabe donde ir a buscarlo
+  lottery = Lottery.attach(addr_); // Los junta
+  tx = await lottery.play(); // Este await se encarga de que espere a que reciba la info de la blockchain
+  console.log(tx);
+}
+
+main();
