923776bb034ecf42f8f024b3e675d148723867ae
IvanSarapura <sarapuraivan25@gmail.com>
2025-07-13 04:15:29 -0300

    Configuracion de hardhat para desplegar en sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
+const { vars } = require("hardhat/config");
+require("dotenv").config();
+
+const INFURA_NODO = process.env.NODO; // Para escribir en la blockchain
+const SEPOLIA_PRIVATE_KEY = process.env.PRKEY; // El que paga el gas
+const ETHERSCAN_API_KEY = process.env.ETHSCAN_KEY; // Va a verificar el contrato
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: INFURA_NODO,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
